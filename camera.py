abd1037d22fe0957f169762b444a625419758e8b
PRIAM <114936255+PPRIAM@users.noreply.github.com>
2022-10-08 16:54:28 +0000

    Initial commit

---
+++ camera.py
@@ -0,0 +1,13 @@
+#--------------------------------------|
+# *The target should be a Rect object  |
+#--------------------------------------|
+
+class Camera:
+  def __init__(self, target):
+    self.target = target
+    self.offset = [0, 0]
+
+  def update(self, screen):
+    self.offset[0] += (self.target.x-self.offset[0]-(screen.get_width()//2+self.target.width//2))
+    self.offset[1] += (self.target.y-self.offset[1]-(screen.get_height()//2+self.target.height//2))
+
---
+++ main.py
@@ -0,0 +1,62 @@
+# Testing file-------------------------
+import pygame, sys, camera
+
+pygame.init()
+FPS = 60
+clock = pygame.time.Clock()
+
+screen = pygame.display.set_mode((500, 500))
+pygame.display.set_caption("PEngine main test")
+
+staticRect = pygame.Rect(10, 10, 10, 10)
+
+playerRect = pygame.Rect(50, 50, 10, 10)
+
+movement = [0, 0]
+
+c = camera.Camera(playerRect)
+
+run = True
+while run:
+    clock.tick(FPS)
+    screen.fill((0, 0, 0))
+
+    c.update(screen)
+    print(c.offset)
+
+    # Draw the static rect
+    staticRect.x + c.offset[0]
+    staticRect.y + c.offset[1]
+    pygame.draw.rect(screen, (255, 0, 0), staticRect)
+
+    # Draw the player rect
+    playerRect.x + c.offset[0]
+    playerRect.y + c.offset[1]
+    pygame.draw.rect(screen, (255, 255, 0), playerRect)
+
+    # Move the player rect
+    if movement[0] == -1:
+        playerRect.x -= 1
+    if movement[0] == 1:
+        playerRect.x += 1
+    if movement[1] == -1:
+        playerRect.y -= 1
+    if movement[1] == 1:
+        playerRect.y += 1
+
+    pygame.display.flip()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+            pygame.quit()
+            sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP: movement[1] -= 1
+            if event.key == pygame.K_DOWN: movement[1] += 1
+            if event.key == pygame.K_LEFT: movement[0] -= 1
+            if event.key == pygame.K_RIGHT: movement[0] += 1
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_UP: movement[1] += 1
+            if event.key == pygame.K_DOWN: movement[1] -= 1
+            if event.key == pygame.K_LEFT: movement[0] += 1
+            if event.key == pygame.K_RIGHT: movement[0] -= 1
